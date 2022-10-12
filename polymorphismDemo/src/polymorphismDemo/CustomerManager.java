1ea1630f50a78615295c5caf0305947f08a906b2
Melih Tezel <melihtezel2001@gmail.com>
2022-10-12 15:26:17 +0300

    3. hafta ödevler

---
+++ polymorphismDemo/src/polymorphismDemo/CustomerManager.java
@@ -0,0 +1,17 @@
+package polymorphismDemo;
+
+public class CustomerManager {
+	 private BaseLogger logger ;
+
+	    public CustomerManager(BaseLogger logger){
+	    this.logger = logger;
+	    }
+
+
+
+
+	    public void Add(){
+	        System.out.println("Müşteri eklendi");
+	        this.logger.Log("log Mesajı");
+	    }
+}
---
+++ polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,10 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+	    customerManager.Add();
+	}
+
+}
